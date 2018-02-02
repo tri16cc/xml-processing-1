4ba502ccfc9d5215db442607a5305a2aa8b7a105
Raluca <ralucam.sandu@gmail.com>
2018-02-02 17:12:37 +0100

    add condtions for each version of CAS file

--- XMLProcessing/extractTPE_1XML.py
+++ XMLProcessing/extractTPE_1XML.py
@@ -5,11 +5,9 @@ Created on Wed Jun 21 18:46:34 2017
 @author: Raluca Sandu
 """
 
-import untangle as ut
-#import os
-#import math
-#import numpy as np
+import numpy as np
 import pandas as pd
+import untangle as ut
 from datetime import datetime
 #%%
 '''Functions for Parsing the XML Structure'''
@@ -28,13 +26,11 @@ def elementExists(node, attr):
 #%%
 #def read_recording_xml(xmlfilename):
       
-#xmlfilename = 'C:/IRE_Stockholm_35cases/10_Pat_KAFAIE MARYAM/Study_1/Series_1/CAS-One Recordings/2015-07-14_09-43-39/Validation_Series_2_12_49_04.xml'
-xmlfilename = 'testCas_2_5_v2.xml'
+xmlfilename = 'multipleLesionsIRE.xml'
 LesionCount = 0 # if the patient has multiple lesions with several IREs
 
 IRE_data = []
 patientID = 'test'
-
 
 try:
     obj = ut.parse(xmlfilename)
@@ -43,7 +39,7 @@ except Exception:
 #    continue
 
 try:
-    data = obj.Eagles.Trajectories.Trajectory
+    trajectories = obj.Eagles.Trajectories.Trajectory
 except Exception:
     print('No trajectory was found in the excel file')
 #    continue
@@ -58,82 +54,74 @@ timeStart, timeEnd = times.split(' ')
 datetime_End = datetime.strptime(year + ' ' + timeEnd, '%Y-%m-%d %H.%M.%S')
 datetime_Start =  datetime.strptime(year + ' ' + timeStart, '%Y-%m-%d %H-%M-%S')
 
-NeedleCount = 1
-LesionCount = 1
+#%%
+NeedleCount = 0
+LesionCount = 0
 # count how many children trajectories are defined --> number of lesions
-#            for trajectory in data:
-#                if (trajectory['type']) and 'IRE' in trajectory['type']:
-#                    LesionCount +=1
-#                    childrenTrajectory = trajectory.Children.Trajectory
-#                    for singletrajectory in ChildrenTrajectory:
-#                        Need 
+for trajectory in trajectories:
+    
+    if (trajectory['type']) and 'IRE' in trajectory['type']:
+        LesionCount +=1
+        childrenTrajectory = trajectory.Children.Trajectory
+    elif not(trajectory['type'] and 'EG_ATOMIC' in trajectory['type']):
+        childrenTrajectory = trajectory
+        LesionCount +=1
+        
+    for singletrajectory in childrenTrajectory:
+        NeedleCount +=1
 
 # count how many trajectories are defined into the children trajectory --> number of needles for each trajectory
 #%%
-for trajectory in data:
+for trajectory in trajectories:
     if (trajectory['type']) and 'IRE' in trajectory['type']:
         epRef = trajectory.EntryPoint.cdata  # entry point coordinates for the reference IRE trajectory
         tpRef = trajectory.TargetPoint.cdata # target point coordinates for the reference IRE trajectory
         # iterate through IRE ChildrenTrajectories
         childrenTrajectory = trajectory.Children.Trajectory
-        for singleTrajectory in childrenTrajectory:
-            if elementExists('singleTrajectory', 'Measurements') is False:
-                print('No Measurement for this needle')
+    elif not(trajectory['type'] and 'EG_ATOMIC' in trajectory['type']):
+        childrenTrajectory = trajectory
+        epRef = ''
+        tpRef = ''
+    else:
+        print('well fuck')
+        continue
+    for singleTrajectory in childrenTrajectory:
+        if elementExists('singleTrajectory', 'Measurements') is False:
+            print('No Measurement for this needle') 
+        else:
+            if elementExists('singleTrajectory.Measurements.Measurement.TPEErrors', 'targetLateral'):
+                targetLateral = singleTrajectory.Measurements.Measurement.TPEErrors['targetLateral'][0:5]
+                targetLongitudinal = singleTrajectory.Measurements.Measurement.TPEErrors['targetLongitudinal'][0:5]
+                targetAngular = singleTrajectory.Measurements.Measurement.TPEErrors['targetAngular'][0:5]
+                targetResidual = singleTrajectory.Measurements.Measurement.TPEErrors['targetResidualError'][0:5]
             else:
-                if elementExists('singleTrajectory.Measurements.Measurement.TPEErrors', 'targetLateral'):
-                    targetLateral = singleTrajectory.Measurements.Measurement.TPEErrors['targetLateral'][0:5]
-                    targetLongitudinal = singleTrajectory.Measurements.Measurement.TPEErrors['targetLongitudinal'][0:5]
-                    targetAngular = singleTrajectory.Measurements.Measurement.TPEErrors['targetAngular'][0:5]
-                    targetResidual = singleTrajectory.Measurements.Measurement.TPEErrors['targetResidualError'][0:5]
-                else:
-                    # the case where the TPE errors are 0 in the TPE<0>. instead they are attributes of the measurement   
-                    targetLateral = singleTrajectory.Measurements.Measurement['targetLateral'][0:5]
-                    targetLongitudinal = singleTrajectory.Measurements.Measurement['targetLongitudinal'][0:5]
-                    targetAngular = singleTrajectory.Measurements.Measurement['targetAngular'][0:5]
-                    targetResidual = singleTrajectory.Measurements.Measurement['targetResidualError'][0:5]
-                    
-                single_measurement_errors = {
-                                     'PatientID': patientID,
-                                     'casVersion': CAS_version,
-                                     'NeedleCount': NeedleCount,
-                                     'LateralError': targetLateral,
-                                     'LongitudinalError': targetLongitudinal,
-                                     'AngularError': targetAngular,
-                                     'ResidualError': targetResidual,
-                                     'entryNeedle_Validation': singleTrajectory.Measurements.Measurement.EntryPoint.cdata,
-                                     'targetNeedle_Validation': singleTrajectory.Measurements.Measurement.TargetPoint.cdata,
-                                     'entryNeedle_Planning': singleTrajectory.EntryPoint.cdata,
-                                     'targetNeedle_Planning': singleTrajectory.TargetPoint.cdata,
-                                     'entryRef': epRef,
-                                     'targetRef': tpRef,
-                                     'datetime_Start':datetime_Start,
-                                     'datetime_End': datetime_End}
-                NeedleCount+=1
-                IRE_data.append(single_measurement_errors)
-            
-    
+                # the case where the TPE errors are 0 in the TPE<0>. instead they are attributes of the measurement   
+                targetLateral = singleTrajectory.Measurements.Measurement['targetLateral'][0:5]
+                targetLongitudinal = singleTrajectory.Measurements.Measurement['targetLongitudinal'][0:5]
+                targetAngular = singleTrajectory.Measurements.Measurement['targetAngular'][0:5]
+                targetResidual = singleTrajectory.Measurements.Measurement['targetResidualError'][0:5]
+                
+            single_measurement_errors = {
+                                 'PatientID': patientID,
+                                 'casVersion': CAS_version,
+                                 'NeedleCount': NeedleCount,
+                                 'LateralError': targetLateral,
+                                 'LongitudinalError': targetLongitudinal,
+                                 'AngularError': targetAngular,
+                                 'ResidualError': targetResidual,
+                                 'entryNeedle_Validation': singleTrajectory.Measurements.Measurement.EntryPoint.cdata,
+                                 'targetNeedle_Validation': singleTrajectory.Measurements.Measurement.TargetPoint.cdata,
+                                 'entryNeedle_Planning': singleTrajectory.EntryPoint.cdata,
+                                 'targetNeedle_Planning': singleTrajectory.TargetPoint.cdata,
+                                 'entryRef': epRef,
+                                 'targetRef': tpRef,
+                                 'datetime_Start':datetime_Start,
+                                 'datetime_End': datetime_End}
+#            NeedleCount+=1
+            IRE_data.append(single_measurement_errors)
+
+        
+        
 #%%                
 df1 = pd.DataFrame(IRE_data)              
-#columns = ['PatientID','CasVersion','time','seriesNo','seriesID','LesionCount', 'needleCount', 'LateralErr', \
-#           'LongitudinalErr', 'AngularErr', 'ResidualErr']
-#
-#df = pd.DataFrame(index=[], columns=columns)    
-#df = df.fillna(0)
-#
-#df['LateralErr'] = Lateral
-#df['LongitudinalErr'] = Longitudinal
-#df['AngularErr'] = Angular
-#df['ResidualErr'] = ResidualError
-#
-#df['PatientID'] = 99
-#df['CasVersion'] = obj.Eagles['version']
-#df['time'] = obj.Eagles['time']
-##df['seriesID'] = obj.Eagles.PatientData['seriesID']
-##df['seriesNo'] = obj.Eagles.PatientData['seriesNumber']
-#df['LesionCount'] = 1
 
-# if new file with IRE  measurement --> check if patient iD exists already
-# if patID exists, append below it (later change to insertion) information
-# increase the number of lesions accordingly
-# write DataFrame to CSV after parsing through all files
-#    
