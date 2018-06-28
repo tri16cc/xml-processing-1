2c592d60761c86a1ec84e55fb648c1334e4a660a
raluca-san <ralucam.sandu@gmail.com>
2018-06-28 12:23:24 +0200

    named tuple return trajectories

--- XMLProcessing/mainExtractTrajectories.py
+++ XMLProcessing/mainExtractTrajectories.py
@@ -6,13 +6,14 @@ Created on Tue Feb 27 16:49:22 2018
 """
 import os
 import re
-import untangle as ut
+import collections
 import pandas as pd
 import NeedlesInfoClass
 import parseIREtrajectories
 import extractTrajectoriesAngles as eta
 from customize_dataframe import customize_dataframe
-#%%
+
+# %%
 # TODO: user keyboard input to ask for study folder
 # TODO: get patient id from the folder name before the intervention
 # TODO: patient naming consistency
@@ -31,37 +32,42 @@ pat_id = 0
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         fileName, fileExtension = os.path.splitext(file)
-        if fileExtension.lower().endswith('.xml') and 'validation'in fileName.lower():
+        if fileExtension.lower().endswith('.xml') and 'validation' in fileName.lower():
             xmlFilePathName = os.path.join(subdir, file)
             xmlfilename = os.path.normpath(xmlFilePathName)
             print(xmlfilename)
             try:
                 # find the numbers before the "_Pat" till the "//"
-                 pat_idx = xmlfilename.find("_Pat")
-                 pat_id_str = re.findall('\\d+', xmlfilename[0:pat_idx])
-                 pat_id = int(pat_id_str[len(pat_id_str)-1])
-                 pat_ids.append(pat_id)
+                pat_idx = xmlfilename.find("_Pat")
+                pat_id_str = re.findall('\\d+', xmlfilename[0:pat_idx])
+                pat_id = int(pat_id_str[len(pat_id_str) - 1])
+                pat_ids.append(pat_id)
             except Exception:
-                 print('numeric data not found in the file name', xmlfilename)
+                print('numeric data not found in the file name', xmlfilename)
             pat_ids.append(pat_id)
             xmlobj = parseIREtrajectories.I_parseRecordingXML(xmlfilename)
             if xmlobj is not None:
-                # parse trajectories
-                # TODO: condition for when the trajectory is none, set the other params as well.
-                trajectories, series, time_intervention, patient_id_xml = parseIREtrajectories.II_parseTrajectories(xmlobj)
-                if trajectories is not None:
+                # parse trajectories and other patient specific info
+                trajectories_info = parseIREtrajectories.II_parseTrajectories(xmlobj)
+                if trajectories_info.trajectories is None:
+                    continue
+                else:
                     # check if patient exists first, if yes, instantiate new object, otherwise retrieve it from list
                     patients = patientsRepo.getPatients()
                     patient = [x for x in patients if x.patientId == pat_id]
                     if not patient:
                         # create patient measurements if patient is not already in the PatientsRepository
-                        patient = patientsRepo.addNewPatient(pat_id, patient_id_xml, time_intervention)
-                        parseIREtrajectories.III_parseTrajectory(trajectories, patient)
+                        patient = patientsRepo.addNewPatient(pat_id,
+                                                             trajectories_info.patient_id_xml,
+                                                             trajectories_info.time_intervention)
+                        # TODO: add series ct to the needle (?!) and also the segmentation path
+                        parseIREtrajectories.III_parseTrajectory(trajectories_info.trajectories, patient)
                     else:
                         # update patient measurements in the PatientsRepository if the patient (id) already exists
-                        parseIREtrajectories.III_parseTrajectory(trajectories, patient[0])
+                        parseIREtrajectories.III_parseTrajectory(trajectories_info.trajectories,
+                                                                 patient[0])
 
-#%% extract information from the object classes into pandas dataframe
+# %% extract information from the object classes into pandas dataframe
 needle_data = []
 patients = patientsRepo.getPatients()
 for p in patients:
@@ -71,15 +77,15 @@ for p in patients:
         NeedlesInfoClass.NeedleToDictWriter.needlesToDict(needle_data, patientID, lIdx, l.getNeedles())
 
 dfPatientsTrajectories = pd.DataFrame(needle_data)
-#dfPatientsTrajectories.sort_values(by=['PatientID'])
-#Angles = []     
-#patient_unique = dfPatientsTrajectories['PatientID'].unique()   
- 
-#%% dataframes for Angles
+# dfPatientsTrajectories.sort_values(by=['PatientID'])
+# Angles = []
+# patient_unique = dfPatientsTrajectories['PatientID'].unique()
+
+# %% dataframes for Angles
 # TODO: flag to cancel Angles if Dataset is MWA
-#for PatientIdx, patient in enumerate(patient_unique):
+# for PatientIdx, patient in enumerate(patient_unique):
 #    patient_data = dfPatientsTrajectories[dfPatientsTrajectories['PatientID'] == patient]
 #    eta.ComputeAnglesTrajectories.FromTrajectoriesToNeedles(patient_data, patient, Angles)
-#dfAngles = pd.DataFrame(Angles) 
+# dfAngles = pd.DataFrame(Angles)
 ## call the customize_dataframe to make columns numerical, write with 2 decimals
-#customize_dataframe(dfAngles, dfPatientsTrajectories, rootdir)
+# customize_dataframe(dfAngles, dfPatientsTrajectories, rootdir)
