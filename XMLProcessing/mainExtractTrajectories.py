c9a846462a9e17032762e171193111a5a77fd7b1
raluca-san <ralucam.sandu@gmail.com>
2018-07-02 17:16:55 +0200

    #4 parse Plan xml and AblationValidation xml

--- XMLProcessing/mainExtractTrajectories.py
+++ XMLProcessing/mainExtractTrajectories.py
@@ -6,7 +6,6 @@ Created on Tue Feb 27 16:49:22 2018
 """
 import os
 import re
-import collections
 import pandas as pd
 import NeedlesInfoClass
 import parseIREtrajectories
@@ -17,11 +16,10 @@ from customize_dataframe import customize_dataframe
 # TODO: user keyboard input to ask for study folder
 # TODO: get patient id from the folder name before the intervention
 # TODO: patient naming consistency
-# TODO: add the CT series number as validation on the XML
+# TODO: extract the datatime from the segmentations folder(new cas version)
 # old cas version: segmentations path read the plan.xml and validation.xml from each segmentations folder to find the corresponding trajectory.
 # new cas version: segmentation path in Ablation_Validation.xml
 # find correct plan/validation based on series number
-# drop the csv segmentation paths because we can't tell which segmentations were used
 rootdir = r"C:\PatientDatasets_GroundTruth_Database\GroundTruth_2018\GT_23042018"
 # instantiate the Patient's Repository class
 patientsRepo = NeedlesInfoClass.PatientRepo()
@@ -32,10 +30,11 @@ pat_id = 0
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         fileName, fileExtension = os.path.splitext(file)
-        if fileExtension.lower().endswith('.xml') and 'validation' in fileName.lower():
+        # TODO: add ablation validation xml type of file
+        if fileExtension.lower().endswith('.xml') and (
+                'validation' in fileName.lower() or 'plan' in fileName.lower()):
             xmlFilePathName = os.path.join(subdir, file)
             xmlfilename = os.path.normpath(xmlFilePathName)
-            print(xmlfilename)
             try:
                 # find the numbers before the "_Pat" till the "//"
                 pat_idx = xmlfilename.find("_Pat")
@@ -60,23 +59,26 @@ for subdir, dirs, files in os.walk(rootdir):
                         patient = patientsRepo.addNewPatient(pat_id,
                                                              trajectories_info.patient_id_xml,
                                                              trajectories_info.time_intervention)
-                        # TODO: add series ct to the needle (?!) and also the segmentation path
-                        parseIREtrajectories.III_parseTrajectory(trajectories_info.trajectories, patient)
+                        parseIREtrajectories.III_parseTrajectory(trajectories_info.trajectories, patient,
+                                                                 trajectories_info.series, xmlfilename)
                     else:
                         # update patient measurements in the PatientsRepository if the patient (id) already exists
-                        parseIREtrajectories.III_parseTrajectory(trajectories_info.trajectories,
-                                                                 patient[0])
-
+                        parseIREtrajectories.III_parseTrajectory(trajectories_info.trajectories, patient[0],
+                                                                 trajectories_info.series, xmlfilename)
 # %% extract information from the object classes into pandas dataframe
 needle_data = []
 patients = patientsRepo.getPatients()
 for p in patients:
     lesions = p.getLesions()
     patientID = p.patientId
-    for lIdx, l in enumerate(lesions):
-        NeedlesInfoClass.NeedleToDictWriter.needlesToDict(needle_data, patientID, lIdx, l.getNeedles())
+    for l_idx, lesion in enumerate(lesions):
+        # some type (list to dict!?) that I haven't figured out how to solve yet
+        needles = lesion.getNeedles()
+        # for each needle get the segmentations associated with it
+        NeedlesInfoClass.NeedleToDictWriter.needlesToDict(needle_data, patientID, l_idx, lesion.getNeedles())
 
 dfPatientsTrajectories = pd.DataFrame(needle_data)
+print("success")
 # dfPatientsTrajectories.sort_values(by=['PatientID'])
 # Angles = []
 # patient_unique = dfPatientsTrajectories['PatientID'].unique()
