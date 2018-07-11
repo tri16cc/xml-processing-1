7684d847f496e40c83f4871c6ee5151cd3323077
raluca-san <ralucam.sandu@gmail.com>
2018-07-11 10:35:14 +0200

    #4 add MWA ellipse info to each trajectory

--- XMLProcessing/mainExtractTrajectories.py
+++ XMLProcessing/mainExtractTrajectories.py
@@ -5,19 +5,15 @@ Created on Tue Feb 27 16:49:22 2018
 @author: Raluca Sandu
 """
 import os
-import re
 import pandas as pd
 import NeedlesInfoClass
 import parseIREtrajectories
 import extractTrajectoriesAngles as eta
 from customize_dataframe import customize_dataframe
+
 # %%
 # TODO: user keyboard input to ask for study folder
-# TODO: patient naming consistency
 # TODO: extract the datatime from the segmentations folder(new cas version)
-# TODO: extract patient id from xml
-# TODO: add the info from the needle database
-# TODO: run on the old folder version
 rootdir = r"C:\PatientDatasets_GroundTruth_Database\GroundTruth_2018\GT_23042018"
 # rootdir = r"C:\PatientDatasets_GroundTruth_Database\Stockholm\3d_segmentation_maverric\maveric"
 
@@ -33,16 +29,15 @@ for subdir, dirs, files in os.walk(rootdir):
             xmlFilePathName = os.path.join(subdir, file)
             xmlfilename = os.path.normpath(xmlFilePathName)
             ## to use only when there is a numeric id in the patient folder
-#            try:
-#                # patient folder naming might differ every time
-#                pat_idx = xmlfilename.find("Pat_")
-#                pat_id_str = re.findall('\\d+', xmlfilename[pat_idx:])
-#                # pat_id = int(pat_id_str[len(pat_id_str) - 1])
-#                pat_id = int(pat_id_str[0])
-#                pat_ids.append(pat_id)
-#            except Exception:
-#                print('numeric data not found in the file name', xmlfilename)
-
+            #            try:
+            #                # patient folder naming might differ every time
+            #                pat_idx = xmlfilename.find("Pat_")
+            #                pat_id_str = re.findall('\\d+', xmlfilename[pat_idx:])
+            #                # pat_id = int(pat_id_str[len(pat_id_str) - 1])
+            #                pat_id = int(pat_id_str[0])
+            #                pat_ids.append(pat_id)
+            #            except Exception:
+            #                print('numeric data not found in the file name', xmlfilename)
             xmlobj = parseIREtrajectories.I_parseRecordingXML(xmlfilename)
 
             if xmlobj is not None:
@@ -55,7 +50,6 @@ for subdir, dirs, files in os.walk(rootdir):
                 if trajectories_info.trajectories is None:
                     continue
                 else:
-                    # TODO: use the patient ID in the XML file
                     # check if patient exists first, if yes, instantiate new object, otherwise retrieve it from list
                     patients = patientsRepo.getPatients()
                     patient = [x for x in patients if x.patientId == pat_id]
@@ -77,28 +71,52 @@ for p in patients:
     lesions = p.getLesions()
     patientID = p.patientId
     for l_idx, lesion in enumerate(lesions):
-        # some type (list to dict!?) that I haven't figured out how to solve yet
         needles = lesion.getNeedles()
         # for each needle get the segmentations associated with it
         NeedlesInfoClass.NeedleToDictWriter.needlesToDict(needle_data, patientID, l_idx, lesion.getNeedles())
-#%%
+# unwrap class object and write to dictionary.
 needle_list = []
 for needles in needle_data:
     for l in needles:
         needle_list.append(l)
-        
-dfPatientsTrajectories = pd.DataFrame(needle_list)     
-print("success")
-#%%
-#filename = 'MAVERRIC_NeedleInfo' + '.xlsx'
-#filepathExcel = os.path.join(rootdir, filename)
-#writer = pd.ExcelWriter(filepathExcel)
-#dfPatientsTrajectories.to_excel(writer, sheet_name='TPEs', index=False, na_rep='NaN')
+df_patients_trajectories = pd.DataFrame(needle_list)
+# %% read MWA Needle Database Excel.
+df_mwa_database = pd.read_excel("CAS_IR_MWA_NeedleDatabase.xlsx")
+
+ellipse_data = []
+for index, row in df_patients_trajectories.iterrows():
+    ablation_index = row["AblationShapeIndex"]
+    ablation_system = row["AblationSystem"]
+    if ablation_index:
+        rows_to_append = df_mwa_database[(df_mwa_database["AblationShapeIndex"] == int(ablation_index)) & (
+                    df_mwa_database["AblationSystem"] == ablation_system)]
+        dict_mwa = rows_to_append.iloc[:, 2:].to_dict('list')
+        ellipse_data.append(dict_mwa)
+    else:
+        dict_mwa = {'NeedleID': '',
+                    'NeedleName': '',
+                    'Power': '',
+                    'Radii': '',
+                    'Rotation': '',
+                    'Time_seconds': '',
+                    'Translation': '',
+                    'Type': ''
+                    }
+        ellipse_data.append(dict_mwa)
+
+# concatenate to df_patients_trajectories
+df_ellipse = pd.DataFrame(ellipse_data)
+df_final = pd.concat([df_patients_trajectories, df_ellipse], axis=1, join_axes=[df_ellipse.index])
+
+# %% TODO: write to excel final list
+# filename = 'MAVERRIC_NeedleInfo' + '.xlsx'
+# filepathExcel = os.path.join(rootdir, filename)
+# writer = pd.ExcelWriter(filepathExcel)
+# dfPatientsTrajectories.to_excel(writer, sheet_name='TPEs', index=False, na_rep='NaN')
 
 # dfPatientsTrajectories.sort_values(by=['PatientID'])
 # Angles = []
 # patient_unique = dfPatientsTrajectories['PatientID'].unique()
-
 # %% dataframes for Angles
 # TODO: flag to cancel Angles if Dataset is MWA
 # for PatientIdx, patient in enumerate(patient_unique):
@@ -107,4 +125,3 @@ print("success")
 # dfAngles = pd.DataFrame(Angles)
 ## call the customize_dataframe to make columns numerical, write with 2 decimals
 # customize_dataframe(dfAngles, dfPatientsTrajectories, rootdir)
-
--- XMLProcessing/parseIREtrajectories.py
+++ XMLProcessing/parseIREtrajectories.py
@@ -58,7 +58,6 @@ def parse_segmentation(singleTrajectory, needle, needle_type, ct_series, xml_fil
         print("segmentation series already exists")
 
     if elementExists(singleTrajectory, 'Ablator'):
-        # TODO: extract the specific MWA info to dict. relate with MWA database log
         needle_params = segmentation.setNeedleSpecifications()
         # TODO: add singleTrajectory.Ablator["readableName"],
         needle_params.setNeedleSpecifications(singleTrajectory.Ablator["id"],
